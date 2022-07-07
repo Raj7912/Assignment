67dc19ce29c23b8de1303642af88e71d5b97613e
Raj7912 <38722007+Raj7912@users.noreply.github.com>
2022-07-07 23:06:38 +0530

    Add files via upload
    area of diffrent shape

---
+++ java/Area_of_tra.java
@@ -0,0 +1,13 @@
+class Area_of_tra
+{
+public static void main(String[] args)
+{
+	double a=12.0,b=10.0,h=12.0,area;
+	area=0.5*(a+b)*h;
+	System.out.println("Area of a trapezoid with parallel sides "+a+" "+b+" and hight "+h+" is = "+area);
+	
+}
+
+
+
+}
