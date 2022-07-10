49b6486e3b7a5eed6abd4b425a32e0b334c03ad5
Purushottam Dey <38722007+Raj7912@users.noreply.github.com>
2022-07-10 10:56:58 +0530

    Create Area_of_cer.java

---
+++ java/global_variable/Area_of_cer.java
@@ -0,0 +1,14 @@
+class Area_of_cer
+{
+  static double pi=3.142,r=12.0,area;
+public static void main(String[] args)
+{
+	
+	area=pi*r*r;
+	System.out.println("Area of a circle with radius "+r+" is = "+area);
+	
+}
+
+
+
+}
